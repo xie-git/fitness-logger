c463ffa62ff4e753492cc7f439c1db639894bb02
Michael Xie <michaelxie@xie-macbook-pro.local>
2024-07-12 14:38:40 -0500

    added second chart

--- app.js
+++ app.js
@@ -127,6 +127,66 @@ document.addEventListener('DOMContentLoaded', function () {
             plugins: [ChartDataLabels]
         });
     }
+
+    function createWorkoutTypeChart(workoutTypeCounts) {
+        const ctx = document.getElementById('workoutTypeChart').getContext('2d');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Push', 'Pull', 'Legs'],
+                datasets: [{
+                    label: 'Workout Type Counts',
+                    data: [workoutTypeCounts.Push, workoutTypeCounts.Pull, workoutTypeCounts.Legs],
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+                    borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(75, 192, 192, 1)'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    x: { 
+                        grid: { 
+                            display: false // Remove the grid lines on the x-axis
+                        }
+                    },
+                    y: { 
+                        beginAtZero: true,
+                        grid: { 
+                            display: false // Remove the grid lines on the y-axis
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false // Optional: Hide the legend
+                    },
+                    tooltip: {
+                        enabled: true // Optional: Enable tooltips
+                    },
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'top',
+                        formatter: function(value) {
+                            return value;
+                        },
+                        color: 'black',
+                        font: {
+                            weight: 'bold'
+                        }
+                    },
+                    title: {
+                        display: true,
+                        text: 'Push, Pull, and Leg Days Count',
+                        font: {
+                            size: 20
+                        }
+                    }
+                }
+            },
+            plugins: [ChartDataLabels]
+        });
+    }
     
     
 
